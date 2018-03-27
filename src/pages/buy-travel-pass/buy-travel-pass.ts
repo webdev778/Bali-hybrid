93f8a0f4431ea58dc76d206b9731a06f503625ab
knight <knight@ksolves.com>
2018-03-27 18:09:00 +0530

    Travel pass CSS fixes

--- src/pages/buy-travel-pass/buy-travel-pass.ts
+++ src/pages/buy-travel-pass/buy-travel-pass.ts
@@ -2,39 +2,39 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 
-
-
  @IonicPage()
  @Component({
  	selector: 'page-buy-travel-pass',
  	templateUrl: 'buy-travel-pass.html',
  })
+
  export class BuyTravelPassPage {
- 	qty:any
- 	count : any[] = [0,0,0]
+ 	arraycount : any[] = [0,0,0]
  	unitCost : any[] = [100,200,300]
  	totalCost : any[] = [0,0,0]
  	finalCost = 0
 
+ 	unitComponent = ['1 Child : $','1 Adult : $','1 Family : $']
+ 	constraintText = ['(Age: 0-15 years)','(Age: 16 and over)','Family (2) Adults and (2) Children']
+ 	indexes = [0,1,2]
+    validityMessage = 'Valid For 60 Days on ARRIVAL'
+    ticketType = ['Child','Adult','Family']
+
  	bundleSaveTickets = { child_ticket_quantity: 0, child_ticket_amount: 0, adult_ticket_quantity: 0, adult_ticket_amount: 0, 
  						  family_ticket_quantity: 0, famliy_ticket_amount: 0, total_amount: 0}
 
-
- 	constructor(public navCtrl: NavController, public navParams: NavParams,private alertCtrl: AlertController) {
- 		
+	constructor(public navCtrl: NavController, public navParams: NavParams,private alertCtrl: AlertController) {
  	}
 
  	ionViewDidLoad() {
  		console.log('ionViewDidLoad BuyTravelPassPage');
- 		console.log(this.count)
+ 		console.log(this.arraycount)
  	}
-
 
  	incrementValue(count,flag) {
  		count++;
 
- 		
- 		this.count[flag] = count;
+ 		this.arraycount[flag] = count;
  		this.totalCost[flag] = count * this.unitCost[flag]
 
  		this.getTotalCost()
@@ -48,37 +48,33 @@ import { AlertController } from 'ionic-angular';
  			count--;
  		}
  		
- 		this.count[flag] = count;
+ 		this.arraycount[flag] = count;
  		this.totalCost[flag] = count * this.unitCost[flag]
  		
  		this.getTotalCost()
 
  	}
 
- 	
  	getTotalCost(){
  		this.finalCost = this.totalCost[0] + this.totalCost[1] + this.totalCost[2];
  		
  	}
 
- 	
- 	initialiseBundle(){
-		this.bundleSaveTickets.child_ticket_quantity = this.count[0];
+	initialiseBundle(){
+		this.bundleSaveTickets.child_ticket_quantity = this.arraycount[0];
  		this.bundleSaveTickets.child_ticket_amount = this.totalCost[0];
 
- 		this.bundleSaveTickets.adult_ticket_quantity = this.count[1];
+ 		this.bundleSaveTickets.adult_ticket_quantity = this.arraycount[1];
  		this.bundleSaveTickets.adult_ticket_amount = this.totalCost[1];
 
- 		this.bundleSaveTickets.family_ticket_quantity = this.count[2];
+ 		this.bundleSaveTickets.family_ticket_quantity = this.arraycount[2];
  		this.bundleSaveTickets.famliy_ticket_amount = this.totalCost[2];
 
  		this.bundleSaveTickets.total_amount = this.finalCost;
  	}
 
-
  	savePressed(){
-
- 		if(this.finalCost == 0){
+		if((this.arraycount[0] == 0) && (this.arraycount[1] == 0) && (this.arraycount[2] == 0)){
  			this.presentAlert();
  		}	
  		else{
